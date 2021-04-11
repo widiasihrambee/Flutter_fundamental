a26c2e767d8afd63b9bb03ec0d256558a081878c
widiasihrambee <widiasihrambe@gmail.com>
2021-04-11 13:21:07 +0700

    Aplikasi pertamaku

---
+++ aplikasi_hello_world/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+//void main() => runApp(MyApp());
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi Hello World"),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                width: 200,
+                height: 100,
+                child: Text(
+                  "Saya sedang Melatih Kemampuan Flutter Saya",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20),
+                ))),
+      ),
+    );
+  }
+}

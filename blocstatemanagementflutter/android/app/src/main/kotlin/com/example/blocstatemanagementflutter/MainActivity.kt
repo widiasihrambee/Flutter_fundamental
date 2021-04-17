009202b80f1af779a2e472789b537247e858aac9
widiasihrambee <widiasihrambe@gmail.com>
2021-04-17 22:31:30 +0700

    Bloc state Management Flutter_Bloc

---
+++ blocstatemanagementflutter/android/app/src/main/kotlin/com/example/blocstatemanagementflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.blocstatemanagementflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ blocstatemanagementflutter/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:blocstatemanagementflutter/color_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider<ColorBloc>(
+          builder: (context) => ColorBloc(), child: MainPage()),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ColorBloc bloc = BlocProvider.of<ColorBloc>(context);
+    return Scaffold(
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            backgroundColor: Colors.amber,
+            onPressed: () {
+              bloc.dispatch(ColorEvent.to_amber);
+            },
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.lightBlue,
+            onPressed: () {
+              bloc.dispatch(ColorEvent.to_light_blue);
+            },
+          ),
+        ],
+      ),
+      appBar: AppBar(
+        title: Text("Bloc Dengan Flutterr_bloc"),
+      ),
+      body: Center(
+        child: BlocBuilder<ColorBloc, Color>(
+          builder: (context, currentColor) => AnimatedContainer(
+            width: 100,
+            height: 100,
+            color: currentColor,
+            duration: Duration(milliseconds: 500),
+          ),
+        ),
+      ),
+    );
+  }
+}
